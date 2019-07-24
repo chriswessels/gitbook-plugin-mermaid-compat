09075bad53130285712e0d75cef1ecd098466414
Chris Alfano <chris@jarv.us>
2019-07-24 19:26:09 +0000

    refactor: move rendering to frontend to avoid glossary conflict

--- dist/book/plugin.js
+++ dist/book/plugin.js
@@ -1,7 +1,21 @@
 require([
-  'gitbook'
-], function (gitbook) {
-  gitbook.events.bind('page.change', function () {
-    mermaid.init();
+  'gitbook',
+  'jquery'
+], function (gitbook, $) {
+  var renderId = 1;
+
+  gitbook.events.bind('start', function () {
+    // configure mermaid
+    mermaid.initialize({
+      startOnLoad: false,
+      theme: 'forest'
+    });
+  });
+
+  gitbook.events.bind('page.change', function (page) {
+    $('.lang-mermaid').each(function() {
+      var svgCode = mermaid.render('mermaid-render-'+renderId++, this.innerText);
+      $(this).closest('pre').replaceWith(svgCode);
+    })
   });
 });
--- index.js
+++ index.js
@@ -1,27 +1,8 @@
-var mermaidRegex = /^```mermaid((.*[\r\n]+)+?)?```$/im;
-
-function processMermaidBlockList(page) {
-
-  var match;
-
-  while ((match = mermaidRegex.exec(page.content))) {
-    var rawBlock = match[0];
-    var mermaidContent = match[1];
-    page.content = page.content.replace(rawBlock, '<div class="mermaid">' +
-      mermaidContent + '</div>');
-  }
-
-  return page;
-}
-
 module.exports = {
   website: {
     assets: './dist',
     js: [
       'book/plugin.js'
     ]
-  },
-  hooks: {
-    'page:before': processMermaidBlockList
   }
 };
